a0db2f1bf07ef5f51f3408e112affb0997e0e120
Kairi OKUMURA <kairi199088@gmail.com>
2015-09-24 00:15:55 +0900

    debugging ac-iosched.c

--- block/ac-iosched.c
+++ block/ac-iosched.c
@@ -7,6 +7,7 @@
  * 4.Distributed Read And Concentrated Write
  */
 #include <linux/kernel.h>
+#include <linux/spinlock.h>
 #include <linux/fs.h>
 #include <linux/blkdev.h>
 #include <linux/elevator.h>
@@ -16,8 +17,9 @@
 #include <linux/init.h>
 #include <linux/compiler.h>
 #include <linux/rbtree.h>
+#include <linux/sched.h>
 
-
+//#define AC_DEBUG
 /*
   Priority
   1.Async WRITE
@@ -32,6 +34,8 @@ static const int async_read_expire = 4 * HZ;
 static const int async_write_expire = 16* HZ;
 
 static const int batch_read = 16;
+static const int fifo_batch = 8;
+static const int writes_starved = 4;
 
 enum{ ASYNC,
 	  SYNC };
@@ -41,30 +45,30 @@ enum Flag { ZERO,
 			PROC,
 			PEND };
 
+
 // cat /sys/class/block/sda/size * QEMU:51584
-static const unsigned long device_sector = ((long)1024 * 50); // QEMU
+//static const unsigned long device_sector = ((long)1024 * 50); // QEMU
+static const unsigned long device_sector = (long)(51584); // QEMU
 
 //static const unsigned long device_sector = ((long)1024 * 1024 * 128); // My Arch
 
-#define FLASH_CHIP_NUM 16
+#define FLASH_CHIP_NUM 8
 #define SQ_NUM FLASH_CHIP_NUM * 2
 
 
+// tmp
 struct sub_queue {
-//	struct rb_root async_read_sorted_list;
-//	struct rb_root async_write_sorted_list;
-//	struct rb_root sync_read_sorted_list;
-//	struct rb_root sync_write_sorted_list;
 	struct rb_root sorted_list[2][2];
-//	struct list_head fifo_list[2][2];
 };
 
 struct ac_matrix {
 	int rq_num[SQ_NUM];
 	int flg[SQ_NUM];
+	//spinlock_t *ac_lock;
 };
 
 struct ac_data {
+	struct request_queue *queue;
 	struct sub_queue sq[SQ_NUM];
 	/* for data */
 	struct ac_matrix matrix[2][2];
@@ -72,15 +76,18 @@ struct ac_data {
 	struct list_head deadline_list[2][2]; // common queue for deadline 
 
 	int pre_mode; // AR or AW or SR or SW
-	int batching;
+	int batched;
+	int starved;
+	int fifo_batch;
 
 	int fifo_expire[2][2];
 	int batch_read;
+	int writes_starved;
 };
 
 
 static inline int ac_get_sq_idx(struct request *rq);
-
+static void ac_display_matrix(struct ac_data *ad, int sync, int data_dir);
 // get expired request
 static struct request *
 ac_expired_request(struct ac_data *ad, int sync, int data_dir) {
@@ -91,7 +98,7 @@ ac_expired_request(struct ac_data *ad, int sync, int data_dir) {
 
 	
 	rq = rq_entry_fifo(list->next);
-//	return rq; // for exp
+	return rq; // for exp
 
 	/* request has expired */
 	if (time_after(jiffies , rq->fifo_time))
@@ -134,10 +141,10 @@ static inline void ac_del_rq_rb(struct ac_data *ad, struct request *rq)
  */
 static inline int ac_get_sq_idx(struct request *rq)
 {
-	const int sector = blk_rq_pos(rq);
-	
-	int sl_sector_num = device_sector / (2 * FLASH_CHIP_NUM) + 1;
+	const int sector = blk_rq_pos(rq); // ok
+	int sl_sector_num = (int)(51584 / SQ_NUM);
 	int idx = (int)(sector / sl_sector_num);
+
 
 	return idx;
 }
@@ -154,7 +161,9 @@ static void ac_add_request(struct request_queue *q, struct request *rq)
 	struct ac_data *ad = q->elevator->elevator_data;
 	struct ac_matrix *mat = &ad->matrix[sync][data_dir];
 
-	printk("KERN_DEBUG insert rq is data_dir:%d, sync:%d\n idx:%d", data_dir, sync, idx);
+#ifdef AC_DEBUG
+	printk("KERN_DEBUG insert rq is data_dir:%d, sync:%d idx:%d\n", data_dir, sync, idx);
+#endif
 	ac_add_rq_rb(ad, rq); // insert tree
 
 	/*
@@ -166,22 +175,20 @@ static void ac_add_request(struct request_queue *q, struct request *rq)
 	// update matrix information
 	mat->rq_num[idx]++;
 	mat->flg[idx] = PEND;
+#ifdef AC_DEBUG	
+	printk("KERN_DEBUG after add rq");
+	ac_display_matrix(ad, ASYNC, READ);
+	ac_display_matrix(ad, ASYNC, WRITE);
+	ac_display_matrix(ad, SYNC, READ);
+	ac_display_matrix(ad, SYNC, WRITE);
+	printk("KERN_DEBUG end after add rq");
+#endif
 }
 
 static int ac_allow_merge(struct request_queue *q, struct request *rq, struct bio *bio)
 {
 	return ELEVATOR_NO_MERGE;
 }
-
-
-static void ac_remove_request(struct request_queue *q, struct request *rq)
-{
-	struct ac_data *ad = q->elevator->elevator_data;
-
-	rq_fifo_clear(rq);
-	ac_del_rq_rb(ad, rq);
-}
-
 
 static void
 ac_update_matrix(struct ac_data *ad, struct request *rq)
@@ -191,7 +198,6 @@ ac_update_matrix(struct ac_data *ad, struct request *rq)
 	const int idx = ac_get_sq_idx(rq);
 	struct ac_matrix *mat = &ad->matrix[sync][data_dir];
 	int i;
-
 	mat->rq_num[idx]--;
 	if (data_dir == READ) { // read
 		if (mat->rq_num[idx] == 0) { 
@@ -231,6 +237,14 @@ ac_update_matrix(struct ac_data *ad, struct request *rq)
 		}
 		
 	}
+
+#ifdef AC_DEBUG
+	printk("KERN_DEBUG ac_update_matrix done.\n");
+	ac_display_matrix(ad, ASYNC, READ);
+	ac_display_matrix(ad, ASYNC, WRITE);
+	ac_display_matrix(ad, SYNC, READ);
+	ac_display_matrix(ad, SYNC, WRITE);
+#endif 
 }
 
 static struct request *
@@ -342,8 +356,9 @@ ac_choose_request(struct ac_data *ad, int data_dir)
 			return rq;
 	}
 
-	// not exist request
+#ifdef AC_DEBUG
 	printk("KERN_DEBUG not exist data_dir:%d  request\n", data_dir);
+#endif
 	return NULL;
 }
 
@@ -354,13 +369,18 @@ static void ac_dispatch_request(struct ac_data *ad, struct request *rq)
 	ac_del_rq_rb(ad, rq);
 	elv_dispatch_add_tail(rq->q, rq);
 
-	//TODO:update matrix index
 	ac_update_matrix(ad, rq);
+
+	if(rq_data_dir(rq))
+		ad->starved = 0;
+	else
+		ad->starved++;
 }
 
+#ifdef AC_DEBUG
 static void ac_display_matrix(struct ac_data *ad, int sync, int data_dir)
 {
-	int i, j;
+	int i;
 	for (i = 0; i < SQ_NUM; i++) {
 		printk("%2d", i);
 	}
@@ -374,6 +394,7 @@ static void ac_display_matrix(struct ac_data *ad, int sync, int data_dir)
 	}
 	printk("\n");
 }
+#endif
 
 static int ac_dispatch_requests(struct request_queue *q, int force)
 {
@@ -381,7 +402,7 @@ static int ac_dispatch_requests(struct request_queue *q, int force)
 	struct request *rq = NULL;
 	int data_dir = READ;
 
-	#ifdef DEBUG_AC
+#ifdef AC_DEBUG
 	printk("KERN_DEBUG dispatch reqests\n");
 	printk("KERN_DEBUG list%d\n", list_empty(&ad->deadline_list[ASYNC][READ]));
 	printk("KERN_DEBUG list%d\n", list_empty(&ad->deadline_list[ASYNC][WRITE]));
@@ -391,22 +412,109 @@ static int ac_dispatch_requests(struct request_queue *q, int force)
 	ac_display_matrix(ad, 0, 1);
 	ac_display_matrix(ad, 1, 0);
 	ac_display_matrix(ad, 1, 1);
-	#endif
+#endif 
 
-	rq = ac_choose_expired_request(ad);
+	if (ad->batched > ad->fifo_batch) {
+		ad->batched = 0;
+		rq = ac_choose_expired_request(ad);
+	}
+
 
 	if(!rq) {
-		rq = ac_choose_request(ad, READ);
+		if (ad->starved > ad->writes_starved)
+			data_dir = WRITE;
+
+		rq = ac_choose_request(ad, data_dir);
 
 		if(!rq)
-			rq = ac_choose_request(ad, WRITE);
+			rq = ac_choose_request(ad, !data_dir);
 
 		if(!rq) 
 			return 0;
 	}
 
+#ifdef AC_DEBUG
+	printk("KERN_DEBUG dispatch reqests\n");
+	printk("KERN_DEBUG dispatch rq:%p\n", rq);
+#endif 
 	ac_dispatch_request(ad, rq);
 	return 1;
+}
+
+
+
+/*
+ * initialize elevator private data (ac_data).
+ */
+static int ac_init_queue(struct request_queue *q, struct elevator_type *e)
+{
+	struct ac_data *ad;
+	struct elevator_queue *eq;
+	int i;
+
+	eq = elevator_alloc(q, e);
+	if (!eq)
+		return -ENOMEM;
+
+	ad = kzalloc_node(sizeof(*ad), GFP_KERNEL, q->node);
+	if (!ad) {
+		kobject_put(&eq->kobj);
+		return -ENOMEM;
+	}
+	
+	eq->elevator_data = ad;
+	ad->queue = q;
+	// init deadline queues
+
+	INIT_LIST_HEAD(&ad->deadline_list[ASYNC][READ]);
+	INIT_LIST_HEAD(&ad->deadline_list[ASYNC][WRITE]);
+	INIT_LIST_HEAD(&ad->deadline_list[SYNC][READ]);
+	INIT_LIST_HEAD(&ad->deadline_list[SYNC][WRITE]);
+
+	// init sub queue list
+	for (i = 0; i < SQ_NUM; i++) {
+		ad->sq[i].sorted_list[ASYNC][READ] = RB_ROOT;
+		ad->sq[i].sorted_list[ASYNC][WRITE] = RB_ROOT;
+		ad->sq[i].sorted_list[SYNC][READ] = RB_ROOT;
+		ad->sq[i].sorted_list[SYNC][WRITE] = RB_ROOT;
+	}
+	// init data matrix
+//	spin_lock_init(ad->matrix[ASYNC][READ].ac_lock);
+//	spin_lock_init(ad->matrix[ASYNC][WRITE].ac_lock);
+//	spin_lock_init(ad->matrix[SYNC][READ].ac_lock);
+
+//	spin_lock_init(ad->matrix[SYNC][WRITE].ac_lock);
+
+	for (i = 0; i < SQ_NUM; i++) {
+		ad->matrix[ASYNC][READ].rq_num[i] = 0;
+		ad->matrix[ASYNC][READ].flg[i] = ZERO;
+		ad->matrix[ASYNC][WRITE].rq_num[i] = 0;
+		ad->matrix[ASYNC][WRITE].flg[i] = ZERO;
+		ad->matrix[SYNC][READ].rq_num[i] = 0;
+		ad->matrix[SYNC][READ].flg[i] = ZERO;
+		ad->matrix[SYNC][WRITE].rq_num[i] = 0;
+		ad->matrix[SYNC][WRITE].flg[i] = ZERO;
+
+	}
+
+	// init preferences
+	ad->pre_mode = READ;
+	ad->batched = 0;
+	ad->starved = 0;
+	ad->fifo_batch = fifo_batch;
+	ad->writes_starved = writes_starved;
+
+	ad->fifo_expire[SYNC][READ] = sync_read_expire;
+	ad->fifo_expire[SYNC][WRITE] = sync_write_expire;
+	ad->fifo_expire[ASYNC][READ] = async_read_expire;
+	ad->fifo_expire[ASYNC][WRITE] = async_write_expire;
+
+	ad->batch_read = batch_read;
+	
+	spin_lock_irq(q->queue_lock);
+	q->elevator = eq;
+	spin_unlock_irq(q->queue_lock);
+	return 0;
 }
 
 static void ac_exit_queue(struct elevator_queue *e)
@@ -429,70 +537,6 @@ static void ac_exit_queue(struct elevator_queue *e)
 	kfree(ad);
 }
 
-/*
- * initialize elevator private data (ac_data).
- */
-static int ac_init_queue(struct request_queue *q, struct elevator_type *e)
-{
-	struct ac_data *ad;
-	struct elevator_queue *eq;
-	int i;
-
-	eq = elevator_alloc(q, e);
-	if (!eq)
-		return -ENOMEM;
-
-	ad = kzalloc_node(sizeof(*ad), GFP_KERNEL, q->node);
-	if (!ad) {
-		kobject_put(&eq->kobj);
-		return -ENOMEM;
-	}
-	
-	eq->elevator_data = ad;
-
-	// init deadline queues
-	INIT_LIST_HEAD(&ad->deadline_list[ASYNC][READ]);
-	INIT_LIST_HEAD(&ad->deadline_list[ASYNC][WRITE]);
-	INIT_LIST_HEAD(&ad->deadline_list[SYNC][READ]);
-	INIT_LIST_HEAD(&ad->deadline_list[SYNC][WRITE]);
-
-	// init sub queue list
-	for (i = 0; i < SQ_NUM; i++) {
-		ad->sq[i].sorted_list[ASYNC][READ] = RB_ROOT;
-		ad->sq[i].sorted_list[ASYNC][WRITE] = RB_ROOT;
-		ad->sq[i].sorted_list[SYNC][READ] = RB_ROOT;
-		ad->sq[i].sorted_list[SYNC][WRITE] = RB_ROOT;
-	}
-
-	// init data matrix
-	for (i = 0; i < SQ_NUM; i++) {
-		ad->matrix[ASYNC][READ].rq_num[i] = 0;
-		ad->matrix[ASYNC][READ].flg[i] = ZERO;
-		ad->matrix[ASYNC][WRITE].rq_num[i] = 0;
-		ad->matrix[ASYNC][WRITE].flg[i] = ZERO;
-		ad->matrix[SYNC][READ].rq_num[i] = 0;
-		ad->matrix[SYNC][READ].flg[i] = ZERO;
-		ad->matrix[SYNC][WRITE].rq_num[i] = 0;
-		ad->matrix[SYNC][WRITE].flg[i] = ZERO;
-	}
-
-	// init preferences
-	ad->pre_mode = READ;
-	ad->batching = 0;
-
-	ad->fifo_expire[SYNC][READ] = sync_read_expire;
-	ad->fifo_expire[SYNC][WRITE] = sync_write_expire;
-	ad->fifo_expire[ASYNC][READ] = async_read_expire;
-	ad->fifo_expire[ASYNC][WRITE] = async_write_expire;
-
-	ad->batch_read = batch_read;
-	
-	spin_lock_irq(q->queue_lock);
-	q->elevator = eq;
-	spin_unlock_irq(q->queue_lock);
-	return 0;
-}
-
 static struct elevator_type iosched_ac = {
 	.ops = {
 		.elevator_allow_merge_fn = ac_allow_merge,
@@ -508,7 +552,8 @@ static struct elevator_type iosched_ac = {
 
 static int __init ac_init(void)
 {
-	return elv_register(&iosched_ac);
+	elv_register(&iosched_ac);
+	return 0;
 }
 
 static void __exit ac_exit(void)
@@ -519,6 +564,6 @@ static void __exit ac_exit(void)
 module_init(ac_init);
 module_exit(ac_exit);
 
-MODULE_AUTHOR("Kairi");
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("ac(alleviate conflict) IO scheduler");
+MODULE_VERSION("0.1");
